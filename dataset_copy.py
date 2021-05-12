401e9a0013fcffd6333ab4cc7a045d5456e8fa75
rohan-dhere <17rohandhere@gmail.com>
2021-05-12 22:32:42 +0530

    rewritten logic src,dest file checking #11
    added function check_file. check if names are same but content is different for two files, check if name and content is same, check file exist or not

--- dataset_copy.py
+++ dataset_copy.py
@@ -1,21 +1,55 @@
+from tqdm import tqdm
+import numpy as np
 import shutil
-from os import path, scandir, remove, cpu_count
+from os import path, scandir, remove, cpu_count, stat
 from queue import Queue
 from threading import Thread
-<<<<<<< HEAD
-import numpy as np
-import filecmp
-from tqdm import tqdm
+
 
 # Number of threads to execute
-cpuCount = cpu_count()
-NO_OF_THREADS = cpuCount
+
+NO_OF_THREADS = cpu_count()
 queue_objects = [Queue() for i in range(NO_OF_THREADS)]
 
-src_list=[]
-dst_list=[]
+src_list = []
+dst_list = []
 
-# TODO File copy code with all exceptions
+
+def check_file(image_path, dest_path):
+    '''
+    Take source destination and copy destination check if file exists 
+
+    Args:
+            image_path(str): Source file path
+            dest_path(str): Destination path
+    Returns:
+            0: File doesn't exist or file exist but content is different
+            1: File exist
+    '''
+
+    try:
+        '''
+        os.stat searches for file, if exists then returns the attributes of file, otherwise OSError 
+        Take size from the attributes of the file
+        '''
+        dest_size = stat(dest_path).st_size
+        # take the size of source file
+        src_size = stat(image_path).st_size
+        if(dest_size == src_size):
+            # same files(including content)
+            print("same files(including content)")
+            return 1
+        else:
+            # same file name, different content
+            print("same file name, different content")
+            return 0
+    except OSError:
+        # file doesn't exist if OSError
+        print("file doesn't exist if OSError")
+        return 0
+
+
+# File copy code
 def copy_data(q):
     '''
     Take source destination from queue and copy to the given destination 
@@ -27,22 +61,18 @@ def copy_data(q):
     pbar = tqdm(total=q.qsize(), position=0, leave=True)
     while not q.empty():
         image_path, dest_path = q.get()
-        # check destination file is regular file and src,dest file name matches
-        if(str(image_path.split("/")[-1]) == str(dest_path.split("/")[-1])):
-            # if src,dest file name same then check content bit by bit
-            if (filecmp.cmp(image_path, dest_path, shallow=True)):
-                pbar.update(1)
-                continue
-            # copy file if doesn't exists
-            else:
-                shutil.copy(image_path, dest_path)
-                pbar.update(1)
+        if (check_file(image_path, dest_path)):
+            pbar.update(1)
+            continue
+        else:
+            # shutil.copy always overights the file if exists at dest
+            shutil.copy(image_path, dest_path)
+            pbar.update(1)
     pbar.close()
 
 
 #  Read all folders recursively
 def read_folders(data_dir, destination_dir):
-
     '''
     Read folders recursively and put files into queues
     Args:
@@ -67,18 +97,21 @@ def read_folders(data_dir, destination_dir):
 
         elif element.is_dir():
             read_folders(element_path, destination_path)
-                        
+
+
 #  reading the destination files
 def read_destination_files(destination_dir):
     folder_elements = scandir(destination_dir)
     for element in folder_elements:
         dst_list.append(element.name)
-        
+
+
 #  checking if source file is deleted if any
-def source_check(dst_list,src_list):
-    diff_list = np.setdiff1d(dst_list,src_list)
+def source_check(dst_list, src_list):
+    diff_list = np.setdiff1d(dst_list, src_list)
     return diff_list
-    
+
+
 #  Reading the full path and deleting the file present in the path
 def delete_from_destination(path_name):
     file_path = path.join(path_name)
@@ -88,8 +121,8 @@ def delete_from_destination(path_name):
 
 if __name__ == '__main__':
 
-    dataset_folder = ""
-    destination_dir = ""
+    dataset_folder = "#"
+    destination_dir = "#"
     read_folders(dataset_folder, destination_dir)
 
     # Start n separate threads
@@ -98,7 +131,5 @@ if __name__ == '__main__':
     read_destination_files(destination_dir)
     source_check()
 
-
     # Calling delete function
     delete_from_destination(dest_img_path)
-
